e48cab5656c1b318e2cbee6e155ff31331dc6d02
Nikolai4ac <nikolai4ac@students.softuni.bg>
2023-11-23 20:48:00 +0200

    Added ejs message for h1

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const indexRouter = express.Router();
 indexRouter.get('/', function(req, res) {
-    res.render('index')
+    res.render('index', {message: "Welcome! Please select an option."})
 })
 
 module.exports = indexRouter;
