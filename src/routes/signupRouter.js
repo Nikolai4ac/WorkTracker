ab4b7321d1b3b26edf9073900f01396089d2ce68
Nikolai4ac <nikolai4ac@students.softuni.bg>
2023-11-24 22:21:11 +0200

    added validation method for password

--- src/routes/signupRouter.js
+++ src/routes/signupRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const signupRouter = express.Router();
-
+const dataValidation = require ('../validations/validateData')
 signupRouter.get('/signup', function(req, res) {
     res.render('signup');
 })
-
 signupRouter.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-
+    // console.log(password); //returns the text from req.body.username
+    console.log(dataValidation.validatePassword(password)); //returns undefined >>> fixed
     if (username.length > 0 && password.length > 0 ) {
         setTimeout(() => {
             res.render('index', {message: "Success!"});
