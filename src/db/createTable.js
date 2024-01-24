f8b09235e0fbe3b0c983f3ee6823852602bd06f0
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2024-01-24 21:07:42 +0200

    tracked folders added

---
+++ src/db/createTable.js
@@ -0,0 +1,24 @@
+const mysql = require ('mysql2');
+const {DBUSER, DBHOST, DBPASSWORD} = require ('../config/config');
+const con = mysql.createConnection({
+    host: DBHOST, 
+    user: DBUSER, 
+    password: DBPASSWORD,
+    database: "myshifts"
+})
+
+con.connect(function(err) {
+    if (err) { 
+        throw err;
+    } else {
+        console.log(`user ${DBUSER} connected`);
+    }
+    const sqlString = "CREATE TABLE shifts (encoded VARCHAR(1), shift_start VARCHAR(4), shift_end VARCHAR(4))";
+    con.query(sqlString, function(err, result) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(`Table created!`);
+        }
+    })
+})
