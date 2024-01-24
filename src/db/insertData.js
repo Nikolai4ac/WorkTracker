19c3d7af47e48414502aef65fcb6003b17fb66cf
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2024-01-24 21:13:14 +0200

    Fixed .js files

--- src/db/insertData.js
+++ src/db/insertData.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require ('mysql2');
 const {DBUSER, DBHOST, DBPASSWORD} = require ('../config/config');
 const con = mysql.createConnection({
@@ -26,33 +25,4 @@ function insertUserData(data) {
         }
     })
 }
-=======
-const mysql = require ('mysql2');
-const {DBUSER, DBHOST, DBPASSWORD} = require ('../config/config');
-const con = mysql.createConnection({
-    host: DBHOST, 
-    user: DBUSER, 
-    password: DBPASSWORD,
-    database: "myshifts"
-})
-
-con.connect(function(err) {
-    if (err) { 
-        throw err;
-    } else {
-        console.log(`user ${DBUSER} connected`);
-    }
-})
-
-function insertUserData(data) {
-    const sqlString = "INSERT INTO shifts (username, password) VALUES (?, ?);";
-    con.query(sqlString, [data[0], data[1]], function(err, result, fields) {
-        if (err) { 
-            throw err;
-        } else {
-            console.log(`Record inserted.`);
-        }
-    })
-}
->>>>>>> b159935c0162220044bc8f203a701d9aa0a871e4
 module.exports = {insertUserData};
