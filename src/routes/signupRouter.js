af11c39621fd3fa1e99426f5e2e7270f4798063f
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2024-01-24 20:54:55 +0200

    Added database insert function, added hashing functionallity for the password

--- src/routes/signupRouter.js
+++ src/routes/signupRouter.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const signupRouter = express.Router();
-const dataValidation = require ('../validations/validateData')
+const dataValidation = require ('../validations/validateData');
+const {insertUserData} = require ('../db/insertData');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 signupRouter.get('/signup', function(req, res) {
     res.render('signup');
 })
 signupRouter.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    // console.log(password); //returns the text from req.body.username
-    console.log(dataValidation.validatePassword(password)); //returns undefined >>> fixed
-    if (username.length > 0 && password.length > 0 ) {
+    let errorPromptMessage = dataValidation.validatePassword(password).msg;
+    let usernameErrorMessage = dataValidation.validateUsername(username).msg;
+    let msgToDisplay = errorPromptMessage + usernameErrorMessage + '\n';
+    if (dataValidation.validatePassword(password).status === "OK" && dataValidation.validateUsername(username).status === "OK") {
+        bcrypt.genSalt(saltRounds, function (err, salt) { 
+            bcrypt.hash(password, salt, function (err, hash) {
+                insertUserData([username, hash]);
+            })
+        })
         setTimeout(() => {
-            res.render('index', {message: "Success!"});
+            res.render('index', {message: `Success! Welcome, ${username}`});
         }, 3000);
+    } else {
+        res.render('signup', {errmsg: msgToDisplay , error: false});
     }
 })
 module.exports = signupRouter;
