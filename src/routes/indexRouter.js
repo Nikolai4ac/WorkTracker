bac2d56baafa2415eaebf796d87e75991850f62c
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2024-01-25 20:53:53 +0200

    Added two messages, default and greet message (for succesfull registration)

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const indexRouter = express.Router();
 indexRouter.get('/', function(req, res) {
-    res.render('index', {message: "Welcome! Please select an option."})
+    const defaultMessage = "Welcome! Please select an option.";
+    const successMessage = req.query.message || defaultMessage;
+    res.render('index', {message: successMessage})
 })
 
 module.exports = indexRouter;
