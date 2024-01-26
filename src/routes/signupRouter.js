c875cf7a696e973117bcafba1e4bf7f707792225
Nikolay Nikolov <Nikolai4ac@students.softuni.bg>
2024-01-26 23:50:36 +0200

    Fixed routing after signup

--- src/routes/signupRouter.js
+++ src/routes/signupRouter.js
@@ -4,8 +4,9 @@ const dataValidation = require("../validations/validateData");
 const { insertUserData } = require("../db/insertData");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+let onUsrSetCallback = null;
 signupRouter.get("/signup", function (req, res) {
-   res.render("signup");
+  res.render("signup");
 });
 signupRouter.post("/signup", (req, res) => {
   const username = req.body.username;
@@ -20,11 +21,19 @@ signupRouter.post("/signup", (req, res) => {
     bcrypt.genSalt(saltRounds, function (err, salt) {
       bcrypt.hash(password, salt, function (err, hash) {
         insertUserData([username, hash]);
+        let usr = username;
+        if (onUsrSetCallback) {
+          onUsrSetCallback(usr);
+        }
       });
     });
-    res.redirect("/?message=Success! Welcome, " + encodeURIComponent(username)); //redirect to the home page
+    res.redirect("/success");
   } else {
     res.render("signup", { errmsg: msgToDisplay, error: false });
   }
 });
-module.exports = signupRouter;
+function onUsrSet(callback) {
+  onUsrSetCallback = callback;
+}
+
+module.exports = { signupRouter, onUsrSet };
